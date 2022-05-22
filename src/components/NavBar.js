ece19d23d08b4f575e5a779c50ae5fa8700b66fd
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-22 03:23:15 -0400

    added draft conditional content

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+// import { useLocation } from 'react-router-dom';
 import '../styles.css';
 
 function NavBar() {
+
+  // const location = useLocation();
+
+  // const currentURL = (url) => {
+  //     return location.pathname.toLowerCase() === url.toLowerCase();
+  // }
+  
+  // const addButton = () => {
+  //   if(currentURL === "/gods/new"){
+  //     return (
+  //       <nav className="nav-menu">
+  //         <NavLink end to="/gods">Gods</NavLink>
+  //         <NavLink end to="/gods/favorites">Favorites</NavLink>
+  //       </nav>
+  //     )
+  //   } else {
+  //     return (
+  //       <nav className="nav-menu">
+  //         <NavLink end to="/gods">Gods</NavLink>
+  //         <NavLink end to="/gods/favorites">Favorites</NavLink>
+  //         <NavLink end to="/gods/new" className='add-god'>Add God</NavLink>
+  //       </nav>
+  //     )
+  //   }
+  // }
+
   return (
       <nav className="nav-menu">
-        <NavLink end to="/">Home</NavLink>
-        <NavLink end to="/gods">All Gods</NavLink>
-        <NavLink end to="/gods/new">New God</NavLink>
+        <NavLink end to="/gods">Gods</NavLink>
+        <NavLink end to="/gods/favorites">Favorites</NavLink>
+        <NavLink end to="/gods/new" className='add-god'>Create God</NavLink>
       </nav>
   );
 }
