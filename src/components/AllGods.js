99bb9cd370a23b301443cc676438bc18e51cc805
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-21 21:01:11 -0400

    Changed location of map

--- src/components/AllGods.js
+++ src/components/AllGods.js
@@ -3,18 +3,15 @@ import '../styles.css';
 import Card from "./Card";
 
 function AllGods({ gods }) {
+
+  const godList = gods.map((god) => <Card key={ god.id } god={ god } />)
   
   return (
     <div> 
     <h1>Guess My Power</h1>
     <p>Click on a god to reveal more details</p>
     <br/>
-    {gods.map((god) => (
-        <Card 
-        key={ god.id } 
-        god={ god }
-       />
-    ))}
+    {godList}
     </div>
   );
 }
