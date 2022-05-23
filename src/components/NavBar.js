5ab433a29037848fdc7a92c8a38b0f98703accae
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-23 02:35:41 -0400

    added home icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
 
   return (
       <nav className="nav-menu">
-        <NavLink end to="/gods"><AiFillHome/></NavLink>
+        <NavLink end to="/"><AiFillHome/></NavLink>
         <NavLink end to="/gods/new" className='add-god'>Create God</NavLink>
       </nav>
   );
