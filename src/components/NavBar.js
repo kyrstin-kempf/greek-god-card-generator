c5347f6e23ca17cd9f515a1312c223c1a4594d84
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-24 23:34:17 -0400

    Added app title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ function NavBar() {
   return (
       <nav className="nav-menu">
         <NavLink end to="/"><AiFillHome/></NavLink>
+        <h1 className="web-title">Greek God Card Generator</h1>
         <NavLink end to="/gods/new" className='add-god'>Create God</NavLink>
       </nav>
   );
